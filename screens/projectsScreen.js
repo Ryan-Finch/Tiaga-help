4e6ea792ee1c749e32b28da0d40eae4b5e5dadfd
ryanwfin <ryan.finch4416@gmail.com>
2020-05-28 13:59:00 -0600

    first commit

--- screens/projectsScreen.js
+++ screens/projectsScreen.js
@@ -8,7 +8,11 @@ import {
   Button,
 } from "react-native";
 import { globalStyles } from "../styles/global";
-import { SearchBar } from "react-native-elements";
+import {ListItem, CheckBox } from "react-native-elements";
+import SearchBarComp from '../components/searchBar'
+import {Collapse, CollapseHeader, CollapseBody} from 'accordion-collapse-react-native'
+import { State } from "react-native-gesture-handler";
+
 
 var DATA = [
   {
@@ -23,6 +27,24 @@ var DATA = [
       name: "Archived",
       color: "#5c3566",
       is_closed: true,
+      },
+    practiceProject:{
+      title: "Project 1",
+      tasks_to_complete:[{
+          task:'Complete Search Bar',
+          isChecked: false,
+          task_description: 'What we need to do'
+        },
+        {
+          task: 'Accordian Results',
+          isChecked: false,
+          task_description: 'Other Stuff We need to do'
+        },
+        {
+          task: 'Whats Next?',
+          isChecked: false,
+          task_description: 'More Stuff to do'
+        }],
     },
     assigned_to: 5,
     assigned_to_extra_info: {
@@ -111,6 +133,24 @@ var DATA = [
       color: "#729fcf",
       is_closed: false,
     },
+    practiceProject:{
+      title: "Project 2",
+      tasks_to_complete:[{
+        task:'Complete Search Bar 2',
+        isChecked: false,
+        task_description: 'What we need to do'
+      },
+      {
+        task: 'Accordian Results 2',
+        isChecked: false,
+        task_description: 'Other Stuff We need to do'
+      },
+      {
+        task: 'Whats Next? 2',
+        isChecked: false,
+        task_description: 'More Stuff to do'
+      }],
+    },
     assigned_to: 5,
     assigned_to_extra_info: {
       username: "johnlund",
@@ -198,6 +238,24 @@ var DATA = [
       color: "#729fcf",
       is_closed: false,
     },
+    practiceProject:{
+      title: "Project 3",
+      tasks_to_complete:[{
+        task:'Complete Search Bar 3',
+        isChecked: false,
+        task_description: 'What we need to do'
+      },
+      {
+        task: 'Accordian Results 3',
+        isChecked: false,
+        task_description: 'Other Stuff We need to do'
+      },
+      {
+        task: 'Whats Next? 3',
+        isChecked: false,
+        task_description: 'More Stuff to do'
+      }],
+    },
     assigned_to: 13,
     assigned_to_extra_info: {
       username: "topher4",
@@ -273,10 +331,59 @@ var DATA = [
 ];
 
 export default function ProjectsScreen() {
+  const [searchResults, getSearchResults] = useState('')
+  const [Data, updateData] = useState(DATA)
+
+  const handleSearchResults= (search)=>{
+    getSearchResults(search)
+  }
+  const handleCheckBox = (index1, index2) =>{
+    console.log(Data[index1].practiceProject.tasks_to_complete[index2].isChecked)
+    updateData([...Data], Data[index1].practiceProject.tasks_to_complete[index2].isChecked = !Data[index1].practiceProject.tasks_to_complete[index2].isChecked)
+  }
+  
+
   return (
     <View>
-      <SearchBar />
-      <Text style={styles.text}></Text>
+      <SearchBarComp
+        handleSearchResults={handleSearchResults} 
+        searchResults={searchResults}
+      />
+      <Text style={styles.text}>Tasks</Text>  
+      {Data.map((d,idx1)=> 
+        {
+          return(
+
+            <Collapse key={idx1}>  
+              <CollapseHeader>
+                <ListItem 
+                  title={d.practiceProject.title}
+                  chevron={{color: 'red'}}
+                  bottomDivider
+                  />
+              </CollapseHeader>
+              <CollapseBody>
+              {d.practiceProject.tasks_to_complete.map((stat, idx2) =>{
+                return (
+                <ListItem 
+                key={idx2}
+                title={stat.task}
+                subtitle={stat.task_description}
+                leftAvatar={
+                  <CheckBox 
+                  key={stat.task}
+                    size={64}
+                    checked={stat.isChecked}
+                    onPress={() => handleCheckBox(idx1, idx2)}
+                  />
+                  }
+                />
+                )})
+              }
+                </CollapseBody>
+            </Collapse>
+        )})
+      } 
     </View>
   );
 }
@@ -288,4 +395,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  container:{
+    padding: 0,
+    width: '100%'
+  }
 });
